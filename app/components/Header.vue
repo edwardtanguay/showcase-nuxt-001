ac973f9638c17fb11a4e87657bf73388ddb70479
Edward Tanguay (windows) <edward@tanguay.info>
2025-09-22 11:41:20 +0200

    FEATURE: Add "Read JSON" page

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,4 +1,18 @@
 <template>
-	<h1 class="text-4xl font-bold mb-3">Info Site</h1>
-	<h2 class="-mt-3 text-xs italic text-yellow-200 mb-3">this site made with <a class="underline" target="_blank" href="https://nuxt.com/docs/4.x/getting-started/introduction">Nuxt 4</a>, see <a class="underline" target="_blank" href="https://github.com/edwardtanguay/showcase-nuxt-001">the code</a></h2>
+  <h1 class="text-4xl text-gray-900 font-bold mb-3">Showcase Nuxt 001</h1>
+  <h2 class="-mt-3 text-xs italic text-gray-900 mb-3">
+    this site made with
+    <a
+      class="underline"
+      target="_blank"
+      href="https://nuxt.com/docs/4.x/getting-started/introduction"
+      >Nuxt 4</a
+    >, see
+    <a
+      class="underline"
+      target="_blank"
+      href="https://github.com/edwardtanguay/showcase-nuxt-001"
+      >the code</a
+    >
+  </h2>
 </template>
---
+++ app/pages/read-json/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+const response = await $fetch("/api/flashcards");
+</script>
+
+<template>
+	<p class="mb-4">This page gets its data from a local JSON file via the Nuxt API and displays a list of flashcards.</p>
+	<div v-for="card in response.data"
+		 :key="card.suuid"
+		 class="flex flex-col gap-1 p-3 mb-3 w-fit bg-gray-700 rounded">
+		<div>
+			<h3 class="text-lg font-semibold">{{ card.front }}</h3>
+			<p class="text-yellow-200">{{ card.back }}</p>
+		</div>
+	</div>
+</template>
