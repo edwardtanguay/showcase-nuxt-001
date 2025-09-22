4d2ff49c02f8992bd101e8c92f0611ce0a05b6f6
Edward Tanguay <edward@tanguay.info>
2025-09-22 10:25:26 +0200

    BASE: Set up Next 4 site

---
+++ app/app.vue
@@ -0,0 +1,13 @@
+<template>
+	<Html>
+		<Head>
+			<Title>Info Site</Title>
+		</Head>
+	</Html>
+	<div class="p-6 bg-slate-600 text-slate-200 min-h-screen">
+	<Header />
+	<Nav/>
+	<NuxtPage />
+	</div>
+
+</template>
---
+++ app/components/Header.vue
@@ -0,0 +1,4 @@
+<template>
+	<h1 class="text-4xl font-bold mb-3">Info Site</h1>
+	<h2 class="-mt-3 text-xs italic text-yellow-200 mb-3">this site made with <a class="underline" target="_blank" href="https://nuxt.com/docs/4.x/getting-started/introduction">Nuxt 4</a>, see <a class="underline" target="_blank" href="https://github.com/edwardtanguay/showcase-nuxt-001">the code</a></h2>
+</template>
