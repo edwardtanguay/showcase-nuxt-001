604c1460bae03e03ab8f93734ac323feba123467
Edward Tanguay (windows) <edward@tanguay.info>
2025-12-17 16:17:14 +0100

    CLEANUP: Combine nuxt4-showcases and this project

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # showcase-nuxt-001
 
+![showcase-nuxt-001](https://github.com/user-attachments/assets/f2f731a8-3eeb-4894-8ee2-ac6b4da63f6d)
+
 This is a showcase site for Nuxt 4.
 
 ## Setup
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,23 @@
+<script setup lang="ts">
+interface WelcomeMessage {
+  title: string;
+  subtitle: string;
+  timestamp: Date;
+}
+
+const message: WelcomeMessage = {
+  title: 'Welcome to this site.',
+  subtitle: 'See the various pages to explore different features.',
+  timestamp: new Date()
+};
+</script>
+
 <template>
-  <p>Welcome to this site.</p>
+  <div>
+    <p>{{ message.title }}</p>
+    <p>{{ message.subtitle }}</p>
+    <p class="text-2xl text-red-900">
+      Loaded at: {{ message.timestamp.toLocaleTimeString() }}
+    </p>
+  </div>
 </template>
